cab70f2fe909d2acf23566e4103e26d445259226
Firoj Nalband <firoj.nalband@neosofttech.com>
2018-06-30 02:45:40 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Project Tracker is a flask application and based on blockchain technology.
+
+This application store an information related an employee and his project into block and block added to blockchain.
+
+
+### Instructions to run
+
+Start a blockchain node server,
+```sh
+>>> python node_server.py
+```
+The application should be up and running at [http://localhost:8000].
+
+
+Run our application,
+
+```sh
+>>> python run_app.py
+```
+The application should be up and running at [http://localhost:5000].
+
---
+++ app/views.py
@@ -0,0 +1,79 @@
+import json, datetime, requests
+
+from app import app
+from flask import render_template, redirect, request
+
+
+# The node with which our application interacts, there can be multiple
+# such nodes as well.
+CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
+
+posts = []
+
+
+def fetch_posts():
+    """
+    Function to fetch the chain from a blockchain node, parse the
+    data and store it locally.
+    """
+    get_chain_address = "{}/chain".format(CONNECTED_NODE_ADDRESS)
+    response = requests.get(get_chain_address)
+    if response.status_code == 200:
+        content = []
+        chain = json.loads(response.content)
+        for block in chain["chain"]:
+            for tx in block["transactions"]:
+                tx["index"] = block["index"]
+                tx["hash"] = block["previous_hash"]
+                content.append(tx)
+
+        global posts
+        posts = sorted(content, key=lambda k: k['timestamp'],
+                       reverse=True)
+
+
+@app.route('/')
+def index():
+    fetch_posts()
+    return render_template('index.html',
+                           title='Transaction',
+                           posts=posts,
+                           node_address=CONNECTED_NODE_ADDRESS,
+                           readable_time=timestamp_to_string)
+
+
+@app.route('/submit', methods=['POST'])
+def submit_textarea():
+    """
+    Endpoint to create a new transaction via our application.
+    """
+    post_object = {
+        'emp_name': request.form["emp_name"],
+        'emp_email': request.form["emp_email"],
+        'emp_phone': request.form["emp_phone"],
+        'project_name': request.form["project_name"],
+        'client_name': request.form["client_name"]
+    }
+
+    # Submit a transaction
+    new_tx_address = "{}/new_transaction".format(CONNECTED_NODE_ADDRESS)
+
+    requests.post(new_tx_address,
+                  json=post_object,
+                  headers={'Content-type': 'application/json'})
+
+    return redirect('/chain')
+
+
+@app.route('/chain')
+def chain():
+    fetch_posts()
+    return render_template('blockchain.html',
+                           title='Blockchain',
+                           posts=posts,
+                           node_address=CONNECTED_NODE_ADDRESS,
+                           readable_time=timestamp_to_string)
+
+
+def timestamp_to_string(epoch_time):
+    return datetime.datetime.fromtimestamp(epoch_time).strftime('%H:%M')
